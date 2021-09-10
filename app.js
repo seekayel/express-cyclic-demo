ce59978c3d540440fec100fc256e05f70f0773d2
Kam <seekayel@users.noreply.github.com>
2021-09-10 08:47:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('*', (req,res) => {
     .set('x-powered-by', 'cyclic.sh')
     .set('content-type', 'application/json')
     .send(JSON.stringify({
-      msg: "Thanks for playing Kevin!",
+      msg: "Message received",
       at: new Date().toISOString(),
       method: req.method,
       hostname: req.hostname,
